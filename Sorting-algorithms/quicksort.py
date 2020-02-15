3d3e2046eca1bfc1596c1699293c6ad7fb23b944
Ankit Gupta <48724966+ankit3466@users.noreply.github.com>
2020-02-15 19:36:04 +0530

    Add files via upload

---
+++ Sorting-algorithms/quicksort.py
@@ -0,0 +1,22 @@
+
+def quick_sort(arr):
+
+    if len(arr)<=1:
+        return arr
+
+    pivot = arr[0]
+    lower,higher,equal = [],[],[]
+    for x in arr:
+        if x<pivot:
+            lower.append(x)
+        elif x==pivot:
+            equal.append(x)
+        else:
+            higher.append(x)
+
+    return quick_sort(lower)+equal+quick_sort(higher)
+
+
+array = [22,1,1,2,34,4,345,645,675,68678,4534,2432,686786,2,543,324]
+
+print(quick_sort(array))
