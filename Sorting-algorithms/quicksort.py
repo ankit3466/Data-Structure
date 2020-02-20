911c47fae69c20b358b7f0100bed720f695d88c7
Ankit Gupta <48724966+ankit3466@users.noreply.github.com>
2020-02-20 21:15:55 +0530

    Update quicksort.py

--- Sorting-algorithms/quicksort.py
+++ Sorting-algorithms/quicksort.py
@@ -17,6 +17,6 @@ def quick_sort(arr):
     return quick_sort(lower)+equal+quick_sort(higher)
 
 
-array = [22,1,1,2,34,4,345,645,675,68678,4534,2432,686786,2,543,324]
+array = [22,1,1,2,34,4,345,645,675,68678,4534]
 
 print(quick_sort(array))
